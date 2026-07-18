0564c042c97ea267c466bcb485352282c1de6db4
Yogitha <yogithakasula@gmail.com>
2026-07-18 11:58:11 +0530

    feat: build initial landing page layout and hero section

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,10 @@
 import { inngest } from "@/lib/inngest/client";
 import { serve } from "inngest/next";
-import { helloWorld } from "@/lib/inngest/functions";
+
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    helloWorld,
+    
   ],
 });
